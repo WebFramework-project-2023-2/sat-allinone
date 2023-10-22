7fb731d6210348961b85140eb33b4718ca0ef9ab
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-22 18:16:45 +0900

    feat: 강의 상세 컴포넌트 생성

---
+++ src/components/CourseDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CourseDetail.css"
+
+function CourseDetail() {
+    return (
+        <div className='background'>
+            <div className='__thumbnail'></div>
+        </div>
+    )
+}
+
+export default CourseDetail
