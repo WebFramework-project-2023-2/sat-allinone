af5a70fb369c34f7a6fe6892ab6f639d134577fc
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-09 18:18:59 +0900

    feat: 더미데이터에서 정보를 가져올 수 있도록 함

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import "./CourseDetail.css"
+import lectureDetail from '../data/lectureDetail.json'
 
 function CourseDetail() {
+    const firstCourse = lectureDetail[0];
+    let id = firstCourse.id;
+    const thumbnailImage = require(`../assets/${id}.png`);
+
     return (
         <div className='background'>
-            <div className='__thumbnail'></div>
-            <div className='__category' id='category'>카테고리 부분</div>
-            <div className='__title' id='title'>*******강의 이름*********</div>
-            <div className='__teacher' id='teacher'>현우진 선생님</div>
+            <div>
+                <img src={thumbnailImage} alt="NO_IMAGE" className='__thumbnail'/>
+            </div>
+            <div className='__category' id='category'>{firstCourse.category}</div>
+            <div className='__title' id='title'>{firstCourse.title}</div>
+            <div className='__teacher' id='teacher'>{firstCourse.teacher}</div>
 
-            <div className='__detail-1'>대상 학년 | 고3·N수</div>
-            <div className='__detail-2'>강좌 구성| 각 40분씩, 총 24강</div>
-            <div className='__detail-3'>제작 방식 | 동영상 스튜디오 강의</div>
-            <div className='__detail-4'>강좌 유형 | 모의고사, 수능(파이널)</div>
-            <div className='__detail-5'>수강 기간 | 59일 (교재배송기간포함)</div>
-            <div className='__detail-6'>부가 기능 | 인덱스 자막</div>
+            <div className='__detail-1'>대상 학년 | {firstCourse.grade}</div>
+            <div className='__detail-2'>강좌 구성 | {firstCourse.structure}</div>
+            <div className='__detail-3'>제작 방식 | {firstCourse.productionMethod}</div>
+            <div className='__detail-4'>강좌 유형 | {firstCourse.type}</div>
+            <div className='__detail-5'>수강 기간 | {firstCourse.duration}</div>
+            <div className='__detail-6'>부가 기능 | {firstCourse.additional}</div>
         </div>
     )
 }
