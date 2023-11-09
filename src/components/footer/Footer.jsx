8e05eac5ac3117f15bfe57eeaeaf202de641006d
Minjae-An <mj3242@naver.com>
2023-11-09 16:52:30 +0900

    feat : Footer 구현

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap"
+import styles from "../../styles/Main.css"
+
+const Footer = () =>{
+    return (
+        <footer className={`${styles.footer} bg-dark text-light`}>
+            <Container>
+                <Row>
+                    <Col md={6} className="text-center">
+                        <h5>수능AllinOne</h5>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6} className="text-center">
+                        <h6>수능AllinOne과 함께 강의 탐색을 시작하세요!</h6>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
+
+export default Footer;
