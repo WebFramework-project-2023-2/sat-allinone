facccb27b86fac8885e71eee2922cbf6740e4092
Minjae-An <mj3242@naver.com>
2023-11-12 19:32:58 +0900

    feat : 강의 목록 이동 버튼 구현

---
+++ src/components/LectureListButton.jsx
@@ -0,0 +1,11 @@
+import {Button} from "react-bootstrap";
+
+const LectureListButton = () => {
+    return (
+        <Button className="lecture-list-button">
+            <span className="lecture-list-button-text">강의 목록 보러가기</span>
+        </Button>
+    );
+}
+
+export default LectureListButton;
