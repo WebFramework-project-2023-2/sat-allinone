ec3e53424c2e57fcb4ce37c94a27a0f46eb6e1ce
Minjae-An <mj3242@naver.com>
2023-11-14 15:48:12 +0900

    feat : 과목 버튼 컴포넌트 수정

--- src/components/SubjectButton.jsx
+++ src/components/SubjectButton.jsx
@@ -2,14 +2,13 @@ import React, {useState} from "react";
 import {Button} from "react-bootstrap";
 
 
-const SubjectButton = (subjectIcon, subjectName) => {
-    const [icon, setIcon] = useState(subjectIcon);
-    const [subject, setSubject] = useState(subjectName);
+const SubjectButton = ({subjectIcon, subjectName}) => {
+    console.log(subjectName);
 
     return (
         <Button variant="primary" className="d-flex align-items-center subjectButton">
-            <img className="subjectButtonIcon" src={require("../assets/icons/subject_sample.png")} alt="subject icon"/>
-            <p className="subjectButtonText">언어와 매체</p>
+            <img className="subjectButtonIcon" src={subjectIcon} alt="subject icon"/>
+            <p className="subjectButtonText">{subjectName}</p>
         </Button>
     );
 }
