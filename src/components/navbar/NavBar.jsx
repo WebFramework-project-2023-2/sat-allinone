062126b9314cf5fad53ee35d547ec83cdb13d075
Minjae-An <mj3242@naver.com>
2023-11-14 15:47:17 +0900

    feat : 내비게이션바 컴포넌트 수정

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import {PATH} from "../../constants/path";
+import {Button} from "react-bootstrap";
+import styles from "../../styles/Main.css"
 
-import * as S from "./style"
-
-const NavBar = () => {
+const NavBar =()=> {
     return (
-        <S.NavBar>
-            <S.NavMenuHome>
-                <S.NavLink to={PATH.HOME}>수능 AllinOne</S.NavLink>
-            </S.NavMenuHome>
-            <S.Wrapper>
-                <S.NavMenu>
-                    <S.NavLink to={PATH.LECTURE_LIST}>강의</S.NavLink>
-                </S.NavMenu>
-                <S.NavMenu>
-                    <S.NavLink to={PATH.RECOMMENDED_LECTURE}>추천 강의</S.NavLink>
-                </S.NavMenu>
-                <S.NavMenu>
-                    <S.NavLink to={PATH.BOOKMARKED_LECTURE}>찜한 강의</S.NavLink>
-                </S.NavMenu>
-            </S.Wrapper>
-            <S.LoginButton>로그인</S.LoginButton>
-        </S.NavBar>
+        <Navbar expand="lg" className={`${styles.nav} bg-body-tertiary justify-content-between`}>
+            <Container>
+                <Navbar.Brand href={PATH.HOME}>수능AllinOne</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href={PATH.LECTURE_LIST}>강의</Nav.Link>
+                        <Nav.Link href={PATH.RECOMMENDED_LECTURE}>추천 강의</Nav.Link>
+                        <Nav.Link href={PATH.BOOKMARKED_LECTURE}>찜한 강의</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <Button type="submit">로그인</Button>
+            </Container>
+        </Navbar>
     );
 }
 
